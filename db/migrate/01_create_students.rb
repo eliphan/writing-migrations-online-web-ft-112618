479b3e16d7c4ccca503010637b12c9441cfd2d30
Elise Phan <aliceconnic@gmail.com>
2019-01-12 04:17:19 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -5,6 +5,13 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def down
   end
   
-  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end 
   
 end
